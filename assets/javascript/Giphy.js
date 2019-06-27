d6b731ef3d08cd3e6de60e8cd93ba868f8882827
Mark Warness <markwarness@gmail.com>
2019-06-27 16:45:45 -0400

    added buttons, calls to api, appended images, etc

--- assets/javascript/Giphy.js
+++ assets/javascript/Giphy.js
@@ -1,45 +1,66 @@
-$(function () {
 
 
-    var dogs = ["English bulldog", "Rottweiler", "French bulldog", "Boston terrier"];
+
+var dogs = ["English bulldog", "Rottweiler", "French bulldog", "Boston terrier"];
+
+
+$("#search-box").click(function () {
+    event.preventDefault();
+    var search = $("#userSearch").val();
+    console.log(search);
+    dogs.push(search);
+    console.log(dogs);
     for (var i = 0; i < dogs.length; i++) {     // parsing with array.length
 
-        $("body").append("<button>" + dogs[i] + "</button>");
-
-
+        $("#buttons").append("<button class = 'newButt' data-value= " + dogs[i] + " >" + dogs[i] + "</button>");
 
 
 
     }
 
-    $("#search-box").click(function () {
-        event.preventDefault();
-        var search = $("#userSearch").val();
-        console.log(search);
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=NQRuLkypf1RM9g18mQTuiYz2BEBrOHTS";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            console.log(response.data[0].rating);
 
-        })
 
-    });
+});
 // 1. user input 2. dogs array 3. array creats buttons of looped variables 4.value of buton into line 15
 
 
 
 
+$(document).on("click", ".newButt", function () {
+    event.preventDefault();
+    var newGif = $(this).attr('data-value');
+    console.log(newGif);
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + newGif + "&api_key=NQRuLkypf1RM9g18mQTuiYz2BEBrOHTS";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.data[0].rating);
+        for (var i = 0; i < response.data.length; i++) {     // parsing with array.length
+// looping
+console.log(response.data[i].images.fixed_height_still.url) 
+// fixed_height_still
+            $("#gifImages").append("<img class = 'addedGif' src=" + response.data[i].images.fixed_height_still.url +" >");
+    
+    
+    
+        }
 
-
-
-
-
-
-
-
-
+    })
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
